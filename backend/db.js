82d6dcabf93d9d53b624935d3e3821953735cce7
shreyaborikar <shreyaborikar02@gmail.com>
2023-12-19 21:41:05 +0530

    first commit

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+// backend/db.js
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/yoga_classes', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
